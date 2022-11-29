88ff7aca9a2feef090607e8ae226828efe70232f
Roy Prins <r.prins@electricant.com>
2022-11-29 15:27:05 +0100

    rename errors

--- packages/validator/src/index.ts
+++ packages/validator/src/index.ts
@@ -3,8 +3,8 @@ const hello = "Hello world";
 export default hello;
 
 interface Feedback {
-  error: string;
   valid: boolean;
+  message: string;
 }
 
 type Validator = (input: string) => Feedback;
@@ -13,34 +13,34 @@ export const min =
   (val: number): Validator =>
   (input) => ({
     valid: input.length >= val,
-    error: `Should contain at least ${val} characters`,
+    message: `Should contain at least ${val} characters`,
   });
 
 export const max =
   (val: number): Validator =>
   (input) => ({
     valid: input.length <= val,
-    error: `Should contain at most ${val} characters`,
+    message: `Should contain at most ${val} characters`,
   });
 
 export const hasUpper: Validator = (input) => ({
   valid: /[A-Z|Ç|Ş|Ğ|Ö|Ü|İ]/u.test(input),
-  error: "Should contain an uppercase letter",
+  message: "Should contain an uppercase letter",
 });
 
 export const hasLower: Validator = (input) => ({
   valid: /[a-z|ç|ş|ğ|ö|ü|ı]/u.test(input),
-  error: "Should contain a lowercase letter",
+  message: "Should contain a lowercase letter",
 });
 
 export const hasNumber: Validator = (input) => ({
   valid: /[0-9]/.test(input),
-  error: "Should contain a number",
+  message: "Should contain a number",
 });
 
 export const email: Validator = (input) => ({
   valid: /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input),
-  error: "Please enter a valid email address",
+  message: "Please enter a valid email address",
 });
 
 export const validation =
